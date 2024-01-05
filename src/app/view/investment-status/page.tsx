377219e2aed2d97fee7e0a5cac06dc4157538622
pjy <wodus331@vanillacode.net>
2024-01-05 11:03:02 +0900

    navbar

---
+++ src/app/view/investment-status/page.tsx
@@ -0,0 +1,5 @@
+import Row from "@/components/layout/Row";
+
+export default function page() {
+  return <Row className={"text-white"}>investment-status</Row>;
+}
--- src/components/share/NavBar.tsx
+++ src/components/share/NavBar.tsx
@@ -3,14 +3,15 @@ import { useConvenienceStore } from "@/store/convenienceStore";
 import AnimatedDiv from "@/components/layout/AnimatedDiv";
 import Col from "@/components/layout/Col";
 import Row from "@/components/layout/Row";
+import Link from "next/link";
 
 export default function NavBar({}) {
   const { getValue } = useConvenienceStore();
 
   const getNavBarStyle = () => {
     return getValue("hideNavBar")
-      ? { marginLeft: "0" }
-      : { marginLeft: "-300px" };
+      ? { marginLeft: "-300px" }
+      : { marginLeft: "0" };
   };
 
   return (
@@ -20,15 +21,52 @@ export default function NavBar({}) {
       }
       style={getNavBarStyle()}
     >
-      <NavBarOption />
+      <NavBarOption
+        title={"주식조회"}
+        option={[
+          { title: "투자현황", url: "/view/investment-status" },
+          { title: "포토폴리오 상세", url: "/view/portfolio" },
+          { title: "누적배당금 내역", url: "/view/accumulated-dividends" },
+          { title: "월별수익률", url: "/view/monthly-returns" },
+          { title: "월간이력", url: "/view/monthly-history" },
+        ]}
+      />
     </AnimatedDiv>
   );
 }
 
-function NavBarOption({}) {
+function NavBarOption({
+  title,
+  option,
+}: {
+  title?: string;
+  option: { title: string; url: string }[];
+}) {
   return (
     <Col>
-      <Row className={"text-neutralGray text-[20px]"}>주식 조회</Row>
+      <Title title={title} />
+      <Col className={"gap-[10px] px-[20px] pt-[10px]"}>
+        {option.map((item, index) => (
+          <Link
+            href={item.url}
+            key={index}
+            className={
+              "cursor-pointer text-[18px] text-neutralGray transition-all duration-200 hover:text-white"
+            }
+          >
+            {item.title}
+          </Link>
+        ))}
+      </Col>
     </Col>
+  );
+}
+
+function Title({ title }: { title?: string }) {
+  if (!title) return;
+  return (
+    <Row className={"px-[10px] pt-[10px] text-[16px] text-neutralGray"}>
+      {title}
+    </Row>
   );
 }
