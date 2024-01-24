9cb0f89bb4d0b265958545d6676f216f31db95ee
GuGura <wodus331@gmail.com>
2024-01-25 02:22:12 +0900

    i18next csr 작업중...

---
+++ src/app/[locale]/i18n/i18n-client.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import i18next from "i18next";
+import {
+  initReactI18next,
+  useTranslation as useTranslationOrg,
+} from "react-i18next";
+import { useCookies } from "react-cookie";
+import resourcesToBackend from "i18next-resources-to-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { getOptions, cookieName } from "./i18-config";
+
+const runsOnServerSide = typeof window === "undefined";
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(
+    resourcesToBackend(
+      (language: string, namespace: string) =>
+        import(`./locales/${language}/${namespace}.json`),
+    ),
+  )
+  .init({
+    ...getOptions(),
+    lng: undefined,
+  });
