19c04feae7e586de795352f44ba8d0f8fdb462ce
GuGura <wodus331@gmail.com>
2024-02-24 23:13:04 +0900

    리펙토링

--- src/app/[locale]/add/portfolio/_components/viewTransaction/ViewTransactionsPage.tsx
+++ src/app/[locale]/add/portfolio/_components/viewTransaction/ViewTransactionsPage.tsx
@@ -10,7 +10,7 @@ import React from "react";
 import { Asset, PortfolioItem } from "@/constant/portfolio";
 
 export default function ViewTransactionsPage() {
-  const { getValue, setValue } = usePortfolio();
+  const { getValue, setValue, transformPortfolio } = usePortfolio();
   const { portfolio, isPending, closePriceData } = usePortfolioData();
 
   const detailSymbol = getValue("data", "detailSymbol");
@@ -23,40 +23,9 @@ export default function ViewTransactionsPage() {
     return item?.asset?.symbol === getValue("data", "detailSymbol");
   });
 
-  const detail: Record<string, PortfolioItem> = list.reduce(
-    (acc: any, cur: Asset) => {
-      const { assetId, transactionType, price, quantity, asset, category } =
-        cur;
-      const symbol = asset.symbol;
-      const name = category.name;
-      const symbolName = asset.name;
-      const adjustedPrice =
-        transactionType === "BUY" ? price * quantity : price * quantity * -1;
-
-      // 기존 항목 업데이트 또는 새 항목 생성
-      const existingItem = acc[symbol] || {
-        price: 0,
-        quantity: 0,
-        symbol,
-        name,
-        symbolName,
-        dailyPrice: -1,
-        prevPrice: -1,
-        updatedAt: null,
-      };
-
-      existingItem.price += adjustedPrice;
-      existingItem.quantity += transactionType === "BUY" ? quantity : -quantity;
-
-      // 일일 가격 데이터 업데이트
-      existingItem.dailyPrice = closePriceData[assetId].dailyClosePrice;
-      existingItem.prevPrice = closePriceData[assetId].prevClosePrice;
-      existingItem.updatedAt = closePriceData[assetId].createdAtDaily;
-
-      acc[symbol] = existingItem;
-      return acc;
-    },
-    {},
+  const detail: Record<string, PortfolioItem> = transformPortfolio(
+    list,
+    closePriceData,
   )[getValue("data", "detailSymbol")];
 
   if (!detailSymbol) {
--- src/store/portfolioStore.ts
+++ src/store/portfolioStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { Asset, PortfolioItem } from "@/constant/portfolio";
 
-interface State {
+interface Props {
   config: {
     isShowChart: boolean;
     portfolioSelected: string | null;
@@ -14,12 +14,10 @@ interface State {
     originTotalPrice: number;
     detailSymbol: any;
   };
-}
-
-interface Action {
   init: (portfolio: any[], closePriceData: any[]) => any;
   getValue: (key: string, ns: string) => any;
   setValue: (key: string, ns: string, value: any) => void;
+  transformPortfolio: (portfolio: any, closePriceData: any) => any;
 }
 
 const initData = {
@@ -30,116 +28,114 @@ const initData = {
   detailSymbol: null,
 };
 
-export const usePortfolioStore = create<State & Action>(
-  (set: any, get: any) => ({
-    config: {
-      isShowChart: true,
-      portfolioSelected: null,
-    },
-    data: {
-      list: null,
-      formattedData: null,
-      dailyTotalPrice: -1,
-      prevTotalPrice: -1,
-      originTotalPrice: -1,
-      detailSymbol: null,
-    },
-    init: (portfolio: any, closePriceData: any) => {
-      set({ data: initData });
-      const list: Record<string, PortfolioItem> = portfolio.reduce(
-        (acc: any, cur: Asset) => {
-          const { assetId, transactionType, price, quantity, asset, category } =
-            cur;
-          const symbol = asset.symbol;
-          const name = category.name;
-          const symbolName = asset.name;
-          const adjustedPrice =
-            transactionType === "BUY"
-              ? price * quantity
-              : price * quantity * -1;
+export const usePortfolioStore = create<Props>((set: any, get: any) => ({
+  config: {
+    isShowChart: true,
+    portfolioSelected: null,
+  },
+  data: {
+    list: null,
+    formattedData: null,
+    dailyTotalPrice: -1,
+    prevTotalPrice: -1,
+    originTotalPrice: -1,
+    detailSymbol: null,
+  },
+  transformPortfolio: (portfolio, closePriceData) => {
+    return portfolio.reduce((acc: any, cur: Asset) => {
+      const { assetId, transactionType, price, quantity, asset, category } =
+        cur;
+      const symbol = asset.symbol;
+      const name = category.name;
+      const symbolName = asset.name;
+      const adjustedPrice =
+        transactionType === "BUY" ? price * quantity : price * quantity * -1;
 
-          // 기존 항목 업데이트 또는 새 항목 생성
-          const existingItem = acc[symbol] || {
-            price: 0,
-            quantity: 0,
-            symbol,
-            name,
-            symbolName,
-            dailyPrice: -1,
-            prevPrice: -1,
-            updatedAt: null,
-          };
+      // 기존 항목 업데이트 또는 새 항목 생성
+      const existingItem = acc[symbol] || {
+        price: 0,
+        quantity: 0,
+        symbol,
+        name,
+        symbolName,
+        dailyPrice: -1,
+        prevPrice: -1,
+        updatedAt: null,
+      };
 
-          existingItem.price += adjustedPrice;
-          existingItem.quantity +=
-            transactionType === "BUY" ? quantity : -quantity;
+      existingItem.price += adjustedPrice;
+      existingItem.quantity += transactionType === "BUY" ? quantity : -quantity;
 
-          existingItem.dailyPrice = closePriceData[assetId].dailyClosePrice;
-          existingItem.prevPrice = closePriceData[assetId].prevClosePrice;
-          existingItem.updatedAt = closePriceData[assetId].createdAtDaily;
+      existingItem.dailyPrice = closePriceData[assetId].dailyClosePrice;
+      existingItem.prevPrice = closePriceData[assetId].prevClosePrice;
+      existingItem.updatedAt = closePriceData[assetId].createdAtDaily;
 
-          acc[symbol] = existingItem;
-          return acc;
-        },
-        {},
+      acc[symbol] = existingItem;
+      return acc;
+    }, {});
+  },
+  init: (portfolio: any, closePriceData: any) => {
+    set({ data: initData });
+    const list: Record<string, PortfolioItem> = get().transformPortfolio(
+      portfolio,
+      closePriceData,
+    );
+
+    const formattedData: PortfolioItem[] = Object.values(list)
+      .filter((item) => item.quantity !== 0)
+      .sort(
+        (a, b) => b?.dailyPrice * b?.quantity - a?.dailyPrice * a?.quantity,
       );
 
-      const formattedData: PortfolioItem[] = Object.values(list)
-        .filter((item) => item.quantity !== 0)
-        .sort(
-          (a, b) => b?.dailyPrice * b?.quantity - a?.dailyPrice * a?.quantity,
-        );
+    const dailyTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
+      acc += cur?.dailyPrice * cur?.quantity;
+      return acc;
+    }, 0.0);
 
-      const dailyTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
-        acc += cur?.dailyPrice * cur?.quantity;
-        return acc;
-      }, 0.0);
-      const prevTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
-        acc += cur?.prevPrice * cur?.quantity;
-        return acc;
-      }, 0.0);
+    const prevTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
+      acc += cur?.prevPrice * cur?.quantity;
+      return acc;
+    }, 0.0);
 
-      console.log(formattedData);
-      const originTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
-        acc += cur?.price;
-        return acc;
-      }, 0.0);
+    const originTotalPrice = formattedData?.reduce((acc: any, cur: any) => {
+      acc += cur?.price;
+      return acc;
+    }, 0.0);
 
-      set({
-        data: {
-          list,
-          formattedData,
-          dailyTotalPrice,
-          prevTotalPrice,
-          originTotalPrice,
-        },
-      });
-    },
-    getValue: (key: string, ns: string) => {
-      switch (key) {
-        case "data":
-          const data = get().data;
-          return data[ns];
-        case "config":
-          const config = get().config;
-          return config[ns];
-      }
-    },
-    setValue: (key: string, ns: string, value: any) => {
-      switch (key) {
-        case "data":
-          const data = get().data;
-          data[ns] = value;
-          set({ data });
-          return;
-        case "config":
-          const config = get().config;
-          config[ns] = value;
-          set({ config });
-          return;
-      }
-    },
-  }),
-);
+    set({
+      data: {
+        list,
+        formattedData,
+        dailyTotalPrice,
+        prevTotalPrice,
+        originTotalPrice,
+      },
+    });
+  },
+  getValue: (key: string, ns: string) => {
+    switch (key) {
+      case "data":
+        const data = get().data;
+        return data[ns];
+      case "config":
+        const config = get().config;
+        return config[ns];
+    }
+  },
+  setValue: (key: string, ns: string, value: any) => {
+    switch (key) {
+      case "data":
+        const data = get().data;
+        data[ns] = value;
+        set({ data });
+        return;
+      case "config":
+        const config = get().config;
+        config[ns] = value;
+        set({ config });
+        return;
+    }
+  },
+}));
 
 export const usePortfolio = () => usePortfolioStore((state) => state);
